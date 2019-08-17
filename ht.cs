6127f97146e7f27404f63b99fe9d00b476b308e1
Unknown <moino.cristian@gmail.com>
2019-08-17 00:38:16 -0600

    commit 2

--- ht.cs
+++ ht.cs
@@ -5,10 +5,10 @@ namespace HelloWorld
     {
         static void Main() 
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola mundo!");
 
             // Keep the console window open in debug mode.
-            Console.WriteLine("Press any key to exit.");
+            Console.WriteLine("\n");
             Console.ReadKey();
         }
     }
