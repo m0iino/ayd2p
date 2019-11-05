b25cb7cf53da9f886af6a7c03ca665192175eb61
Unknown <moino.cristian@gmail.com>
2019-11-04 22:32:04 -0600

    commit webservice2
    webservice2

---
+++ Webservice2/Webservice2/DatosMesa.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Webservice2
+{
+    public class DatosMesa
+    {
+        public DataSet getMesas()
+        {
+            DataSet dataTable = new DataSet();
+
+            //A TRAVEZ DE LA CADENA DE CONEXION DEL WEBCONFIG Y LA OBTENEMOS  
+            //CON EL CONFIGURATIONMANAGER 
+            using (SqlConnection con = new SqlConnection(
+                ConfigurationManager.ConnectionStrings["connectionLocal"]
+                .ConnectionString))
+            {
+                //SE ABRE LA CONEXION 
+                con.Open();
+
+                //SE UTILIZA PARA LLENAR UN DATASET CON LOS ELEMENTOS NECESARIOS  
+                //COMO UNA CONEXION DE BASE DE DATOS 
+                using (SqlDataAdapter sqlAdapter = new SqlDataAdapter(
+                    "SELECT * FROM Mesa", con))
+                {
+                    //SE LLENA EL DATASET CON LOS DATOS OBTENIDOS EN EL SQLADAPTER 
+                    sqlAdapter.Fill(dataTable);
+                }
+            }
+            //REGRESAMOS LOS DATOS COMO DATOS EN MEMORIA 
+            return dataTable;
+        }
+    }
+    
+}
---
+++ Webservice2/Webservice2/WebService1.asmx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml.Serialization;
+namespace Webservice2
+{
+    /// <summary>
+    /// Descripción breve de WebService1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+        DatosMesa mesas = new DatosMesa();
+        [WebMethod]
+        public DataSet getMesas() {
+            return mesas.getMesas();
+        }
+    }
+}
